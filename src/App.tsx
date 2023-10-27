0ba28f5d3d8bbfc739c47b923486c04a3f90107e
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-10-27 17:00:15 +0700

    feat(routing): add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
 function App() {
-
   return (
-    <>
-      <p className="text-red-700">
-        Hello
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element= {<Login />}/>
+        <Route path="/login" element= {<Login />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
