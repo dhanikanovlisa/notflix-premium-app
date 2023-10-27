ec451867d3b649027390a5c351a23df861389d53
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-10-27 17:00:27 +0700

    feat(navbar): add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Navbar() {
+  const [open, isOpened] = useState(false);
+
+  return (
+    <nav className="fixed w-full z-20 justify-between items-center">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-5">
+        <a href="/">
+          <img src="/src/assets/notflix-premium-logo.svg" alt="Logo"></img>
+        </a>
+        <div className="flex flex-row space-x-9 items-center">
+          <a href="/submission">
+            <p className="">Submission</p>
+          </a>
+          <button onClick={() => isOpened(!open)}>
+            <img
+              src="/src/assets/profile-placeholder.png"
+              className="rounded-full shadow-md red-glow"
+              alt="Profile"
+            ></img>
+          </button>
+          {open && (
+            <div className="absolute top-24 right-12 w-36 h-24 bg-white rounded-lg shadow-lg">
+              <ul className="flex flex-col space-y-3 p-4">
+                <li>
+                  <a href="/profile">
+                    <p className="text-black font-bold">Profile</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="/logout">
+                    <p className="text-black font-bold">Log Out</p>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
