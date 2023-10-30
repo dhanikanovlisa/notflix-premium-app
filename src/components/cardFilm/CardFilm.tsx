de59de86858b76ead7df1923aa40893b010cdc92
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-10-30 20:45:15 +0700

    fix(field, card component)

--- src/components/cardFilm/CardFilm.tsx
+++ src/components/cardFilm/CardFilm.tsx
@@ -1,22 +1,17 @@
 interface CardFilmProps {
-    title: string;
-    image?: string;
-  }
-  
-  function CardFilm({ title, image }: CardFilmProps) {
-    return (
-      <div className="flex flex-col items-center">
-        <div className="w-52 h-64 red-glow rounded-md overflow-hidden">
-          <img
-            src={image}
-            alt={title}
-            className="w-full h-full object-cover"
-          />
-        </div>
-        <h3 className="mt-2">{title}</h3>
+  title: string;
+  image?: string;
+}
+
+function CardFilm({ title, image }: CardFilmProps) {
+  return (
+    <div className="w-48 h-full">
+      <div className="w-48 h-60 red-glow rounded-md overflow-hidden">
+        <img src={image} alt={title} className="w-full h-full object-cover" />
       </div>
-    );
-  }
-  
-  export default CardFilm;
-  
+      <h3 className="mt-2 text-white text-center">{title}</h3>
+    </div>
+  );
+}
+
+export default CardFilm;
