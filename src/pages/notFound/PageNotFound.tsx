9d2f2ef382d88cf98670962361b62e48269cd202
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-11-04 17:43:53 +0700

    feat(notfound): add not found

---
+++ src/pages/notFound/PageNotFound.tsx
@@ -0,0 +1,14 @@
+function PageNotFound() {
+  return (
+    <div className="container">
+      <button
+        className="button-red button-text red-glow"
+        aria-label="Back To Home"
+      >
+        Back to Home
+      </button>
+    </div>
+  );
+}
+
+export default PageNotFound;
