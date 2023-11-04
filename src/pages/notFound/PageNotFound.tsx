36071f357221c2cdde6b9d2aff52d21df836b041
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-11-04 22:12:48 +0700

    add(profile): fetch preview + edit profile

--- src/pages/notFound/PageNotFound.tsx
+++ src/pages/notFound/PageNotFound.tsx
@@ -1,11 +1,13 @@
 function PageNotFound() {
   return (
-    <div className="container">
+    <div className="pt-28 pl-10 pr-28 justify-center items-center text-center space-y-4">
+      <h2>Back to Manage Film</h2>
       <button
         className="button-red button-text red-glow"
-        aria-label="Back To Home"
+        aria-label="Back To Manage Film"
+        onClick={() => {window.location.href = "/manage-film";}}
       >
-        Back to Home
+        Back to Manage Film
       </button>
     </div>
   );
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,50 +1,81 @@
 import Navbar from "../../components/navbar/Navbar";
-
+import { useParams } from "react-router-dom";
+import { User } from "../../interfaces/interfaces";
+import { useEffect, useState } from "react";
 function Profile() {
+  const { id } = useParams();
+  const url = import.meta.env.VITE_REST_URL;
+  const [profile, setProfile] = useState<User | undefined>();
 
-  function editProfile(){
-    window.location.href = "/edit-profile"
+  async function getProfile() {
+    const response = await fetch(`${url}/profile/${id}`);
+    const data = await response.json();
+    if (!response.ok) {
+      if (response.status === 404) {
+        setProfile(undefined);
+        window.location.href = "/not-found";
+        return;
+      }
+    }
+    const mappedProfile = data.data;
+    setProfile(mappedProfile);
   }
-  
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  function editProfile() {
+    window.location.href = "/edit-profile/" + id;
+  }
+
   return (
     <>
-      <Navbar />
-      <div className="pt-28 pl-10">
-        <div className="">
-          <h1>Profile Settings</h1>
-        </div>
-        <div className="flex flex-row gap-36 pt-8">
-          <div>
-            <div className="w-48 h-48">
-              <img
-                src="/src/assets/profile-placeholder.png"
-                className="rounded-full object-cover w-full h-full"
-              />
+      {profile && (
+        <>
+          <Navbar />
+          <div className="pt-28 pl-10">
+            <div className="">
+              <h1>Profile Settings</h1>
+            </div>
+            <div className="flex flex-row gap-36 pt-8">
+              <div>
+                <div className="w-48 h-48">
+                  <img
+                    src={`/src/assets/storage/profile/${profile.photo_profile}`}
+                    className="rounded-full object-cover w-full h-full"
+                    alt = "Profile picture"
+                  />
+                </div>
+              </div>
+              <div className="space-y-8">
+                <div className="username-container">
+                  <h3>Username</h3>
+                  <p>{profile?.username}</p>
+                </div>
+                <div className="username-container">
+                  <h3>Name</h3>
+                  <p>{profile?.first_name} {profile?.last_name} </p>
+                </div>
+                <div className="username-container">
+                  <h3>Email</h3>
+                  <p>{profile.email}</p>
+                </div>
+                <div className="username-container">
+                  <h3>Phone Number</h3>
+                  <p>{profile.phone_number}</p>
+                </div>
+                <button
+                  className="font-bold button-white"
+                  onClick={editProfile}
+                >
+                  Edit Profile
+                </button>
+              </div>
             </div>
           </div>
-          <div className="space-y-8">
-            <div className="username-container">
-              <h3>Username</h3>
-              <p>Username</p>
-            </div>
-            <div className="username-container">
-              <h3>Name</h3>
-              <p>Dhanika Novlisariyanti</p>
-            </div>
-            <div className="username-container">
-              <h3>Email</h3>
-              <p>email@gmail.com</p>
-            </div>
-            <div className="username-container">
-              <h3>Phone Number</h3>
-              <p>phone</p>
-            </div>
-            <button className="font-bold button-white" onClick={editProfile}>
-              Edit Profile
-            </button>
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 }
