900cf6e5493b50d11caa432d88d213f0ae8102fb
dhanikanovlisa <13521132@std.stei.itb.ac.id>
2023-11-13 19:27:55 +0700

    fix route

--- src/components/protectedRoutes/ProtectedRoutes.tsx
+++ src/components/protectedRoutes/ProtectedRoutes.tsx
@@ -1,59 +1,63 @@
-import { useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 type ProtectedRouteProps = {
-  type?: string
-  children: JSX.Element
-}
+  type?: string;
+  children: JSX.Element;
+};
 
 const url = import.meta.env.VITE_REST_URL;
 
 const ProtectedRoute = ({ type, children }: ProtectedRouteProps) => {
-const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [id, setId] = useState<number>(0);
   const fetchData = async () => {
     try {
-        const res = await fetch(`${url}/check/current-user`, {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: 'Bearer ' + localStorage.getItem('token') || '',
-            },
-        });
+      const res = await fetch(`${url}/check/current-user`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token") || "",
+        },
+      });
 
-        const data = await res.json();
+      const data = await res.json();
+      if (data.user) {
+        setId(Number(localStorage.getItem("id")));
         console.log(data);
 
-        if (!data.isAuth){
-            navigate("/login");
+        if (!data.isAuth) {
+          navigate("/login");
         }
 
-        if (type && type.includes('admin') && !data.user.is_admin){
-            navigate("/not-found");
+        if (type && type.includes("admin") && !data.user.is_admin) {
+          navigate("/not-found");
         }
 
-        if (type && type.includes('user') && data.user.is_admin){
-            navigate("/not-found");
+        if (type && type.includes("user") && data.user.is_admin) {
+          navigate("/not-found");
         }
 
-        if (type && type.includes('home') && !data.user.is_admin){
-            navigate("/submission");
-        }        
-        
-        // nunggu
-        // if (type && type.includes('admin') && type.includes('home')){
-        //     navigate("/substription");
-        // }
+        if (type && type.includes("home") && !data.user.is_admin) {
+          navigate("/submission/" + id);
+        }
 
+        if (type && type.includes("admin") && type.includes("home")) {
+          navigate("/substription");
+        }
+      } else {
+        navigate("/login");
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])     
+    fetchData();
+  }, []);
 
   return children;
-}
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
