60a6be2423d1e4639fc7ea3de7b5db90e0c590a4
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-11-17 09:59:37 +0700

    style(cardApproval): card styling

--- src/components/card/CardApproval.tsx
+++ src/components/card/CardApproval.tsx
@@ -7,25 +7,25 @@ interface CardProps {
 
 function CardApproval({ title, description, onAccept, onReject }: CardProps) {
   return (
-      <div className="w-72 sm:w-96 md:w-96 lg:w-96 h-full red-glow p-6 rounded-md flex flex-wrap flex-col sm:flex-row md:flex-row lg:flex-row sm:space-x-6 md:space-x-6 lg:space-x-6 sm:items-center md:items-center lg:items-center">
+      <div className="w-full sm:w-full md:w-10/12 lg:w-96 red-glow p-6 rounded-md flex flex-wrap flex-col justify-between">
         <div className="mb-4">
-          <h3 className="text-left">{title}</h3>
+          <h3 className="text-left mb-4">{title}</h3>
           <p className="text-left text-sm"> {description}</p>
         </div>
-        <div className="flex flex-row space-x-3">
+        <div className="flex flex-row space-x-3 justify-center">
           <button
             type="button"
             className="text-white h-10 font-bold button-red hover:bg-red-800"
             onClick={onReject}
           >
-            Cancel
+            Reject
           </button>
           <button
             type="button"
             className="text-white h-10 font-bold bg-green-600 hover:bg-green-800 "
             onClick={onAccept}
           >
-            Save
+            Accept
           </button>
         </div>
       </div>
